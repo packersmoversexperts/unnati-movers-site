3e05ff277eb710fe4319f14ca203fe9728a28fc7
packersmoversexperts <packersmoversexperts@gmail.com>
2025-08-21 23:16:40 +0530

    Create config.php

---
+++ partials/includes/config.php
@@ -0,0 +1,4 @@
+<?php
+// This file will be auto-written by setup.php during installation.
+$DB_HOST='localhost'; $DB_USER=''; $DB_PASS=''; $DB_NAME='';
+$SITE_URL=''; $ADMIN_EMAIL='leads.unnatimovers@gmail.com';
